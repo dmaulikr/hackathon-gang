09ae62a7fdbe6054a5ec60a0cae75a9f51abdcd2
Shabbir Vijapura <shabbirv@gmail.com>
2013-07-13 17:44:33 -0500

    FINISHED

---
+++ Gang/Pods/Pods-environment.h
@@ -0,0 +1,38 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// AFNetworking
+#define COCOAPODS_POD_AVAILABLE_AFNetworking
+#define COCOAPODS_VERSION_MAJOR_AFNetworking 1
+#define COCOAPODS_VERSION_MINOR_AFNetworking 3
+#define COCOAPODS_VERSION_PATCH_AFNetworking 1
+
+// AKTabBarController
+#define COCOAPODS_POD_AVAILABLE_AKTabBarController
+#define COCOAPODS_VERSION_MAJOR_AKTabBarController 1
+#define COCOAPODS_VERSION_MINOR_AKTabBarController 1
+#define COCOAPODS_VERSION_PATCH_AKTabBarController 0
+
+// CollapseClick
+#define COCOAPODS_POD_AVAILABLE_CollapseClick
+#define COCOAPODS_VERSION_MAJOR_CollapseClick 1
+#define COCOAPODS_VERSION_MINOR_CollapseClick 0
+#define COCOAPODS_VERSION_PATCH_CollapseClick 1
+
+// SVProgressHUD
+#define COCOAPODS_POD_AVAILABLE_SVProgressHUD
+#define COCOAPODS_VERSION_MAJOR_SVProgressHUD 0
+#define COCOAPODS_VERSION_MINOR_SVProgressHUD 9
+#define COCOAPODS_VERSION_PATCH_SVProgressHUD 0
+
+// UIColor+MLPFlatColors
+#define COCOAPODS_POD_AVAILABLE_UIColor_MLPFlatColors
+#define COCOAPODS_VERSION_MAJOR_UIColor_MLPFlatColors 1
+#define COCOAPODS_VERSION_MINOR_UIColor_MLPFlatColors 0
+#define COCOAPODS_VERSION_PATCH_UIColor_MLPFlatColors 0
+
